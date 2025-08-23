02aa2db6f86e730ff987aeeec2a9ac730467c77c
SharickGPinto <pinto702rodri@gmail.com>
2025-08-23 17:59:51 -0500

    feat: :sparkles: Add codes script

---
+++ script.js
@@ -0,0 +1,39 @@
+async function iniciar() {
+    try {
+        const res = await fetch('https://68aa2325909a5835049bb9f8.mockapi.io/USUARIO', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        let data = await res.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function entrar(){
+    const emailInput = document.getElementById('login_email').value.trim();
+    const passInput = document.getElementById('login_password').value.trim();
+
+    const data = await iniciar();
+    data.forEach(i  => {
+        if (i.Usuario === emailInput && i.contraseya === passInput){
+            usuariarito = i
+        }
+    });
+    if (usuariarito){
+        localStorage.setItem('usuarito', JSON.stringify(usuariarito));
+        if(usuariarito.type === 'Estudiante'){
+            window.location.href = '../../pages/estudiantes/panelE.html';
+        }else if (usuariarito.type === 'Docente'){
+            window.location.href = './pages/docente/panelD.html';
+        }else if (usuariarito.type === 'Administrador'){
+            window.location.href = './pages/administrador/';
+        }
+    }else{
+        alert("Usuario o contraseña incorrecta")
+    }
+}
